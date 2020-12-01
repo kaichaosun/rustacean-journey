3505ce61a641908cd2553a102f5d5a5273160673
Kaichao Sun <kaichaosuna@gmail.com>
2020-12-01 22:00:45 +0800

    derive macro demo

--- macros/proc-macro-demo/proc-macro-example/src/lib.rs
+++ macros/proc-macro-demo/proc-macro-example/src/lib.rs
@@ -2,5 +2,10 @@ use proc_macro::TokenStream;
 
 #[proc_macro]
 pub fn make_answer(_item: TokenStream) -> TokenStream {
-    "fn answer() -> u32 { 42 }".parse().unwrap()
+    "fn answer_func() -> u32 { 42 }".parse().unwrap()
+}
+
+#[proc_macro_derive(AnswerFn)]
+pub fn derive_answer_fn(_item: TokenStream) -> TokenStream {
+    "fn answer_derive() -> u32 { 42 }".parse().unwrap()
 }
--- macros/proc-macro-demo/src/main.rs
+++ macros/proc-macro-demo/src/main.rs
@@ -1,7 +1,12 @@
 use proc_macro_example::make_answer;
+use proc_macro_example::AnswerFn;
 
 make_answer!();
 
+#[derive(AnswerFn)]
+struct MyStruct;
+
 fn main() {
-    println!("{}", answer());
+    println!("{}", answer_func());
+    println!("{}", answer_derive());
 }
