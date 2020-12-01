b628b992ccedfe3b7eeeeded363aeffebf8ad099
Kaichao Sun <kaichaosuna@gmail.com>
2020-12-01 22:16:49 +0800

    Attribute macros demo

--- macros/proc-macro-demo/proc-macro-example/src/lib.rs
+++ macros/proc-macro-demo/proc-macro-example/src/lib.rs
@@ -1,11 +1,32 @@
 use proc_macro::TokenStream;
 
+// Function-like procedural macros
 #[proc_macro]
 pub fn make_answer(_item: TokenStream) -> TokenStream {
     "fn answer_func() -> u32 { 42 }".parse().unwrap()
 }
 
+// Derive macros
 #[proc_macro_derive(AnswerFn)]
 pub fn derive_answer_fn(_item: TokenStream) -> TokenStream {
     "fn answer_derive() -> u32 { 42 }".parse().unwrap()
+}
+
+// Derive macro with helper attributes
+#[proc_macro_derive(HelperAttr, attributes(helper))]
+pub fn derive_helper_attr(_item: TokenStream) -> TokenStream {
+    TokenStream::new()
+}
+
+// Attribute macros
+#[proc_macro_attribute]
+pub fn return_as_is(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
+
+#[proc_macro_attribute]
+pub fn show_streams(attr: TokenStream, item: TokenStream) -> TokenStream {
+    println!("attr: \"{}\"", attr.to_string());
+    println!("item: \"{}\"", item.to_string());
+    item
 }
--- macros/proc-macro-demo/src/main.rs
+++ macros/proc-macro-demo/src/main.rs
@@ -1,10 +1,29 @@
 use proc_macro_example::make_answer;
 use proc_macro_example::AnswerFn;
+use proc_macro_example::HelperAttr;
+use proc_macro_example::show_streams;
 
 make_answer!();
 
 #[derive(AnswerFn)]
 struct MyStruct;
+
+#[derive(HelperAttr)]
+struct MyStructAttr {
+    #[helper] field: ()
+}
+
+#[show_streams]
+fn invoke1() {}
+
+#[show_streams(bar)]
+fn invoke2() {}
+
+#[show_streams(multiple => tokens)]
+fn invoke3() {}
+
+#[show_streams { delimiters} ]
+fn invoke4() {}
 
 fn main() {
     println!("{}", answer_func());
