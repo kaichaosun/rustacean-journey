6d5a5f8b2d700ca3475c5534abaffcfa96e7beeb
Kaicho Sun <kaichaosuna@gmail.com>
2022-06-27 11:59:36 +0800

    mutex to lock and mutate the value.

--- arc/src/main.rs
+++ arc/src/main.rs
@@ -1,22 +1,28 @@
-use std::{thread::spawn, sync::Arc};
+// resource: https://itsallaboutthebit.com/arc-mutex/#
+
+use std::{thread::{spawn, sleep}, sync::{Arc, Mutex}, time::Duration};
 
 struct User {
     name: String,
 }
 
 fn main() {
-    let user_original = Arc::new(User {
+    let user_original = Arc::new(Mutex::new(User {
         name: "kaichao".to_string(),
-    });
+    }));
 
     let user = user_original.clone();
     let t1 = spawn(move || {
-        println!("Hello from the first thread {}", user.name);
+        let mut locked_user = user.lock().unwrap();
+        println!("Hello from the first thread {}", locked_user.name);
+        locked_user.name = String::from("jack");
+        println!("Hello from the first thread {}", locked_user.name);
     });
 
     let user_t2 = user_original.clone();
     let t2 = spawn(move || {
-        println!("Hello from the second thread {}", user_t2.name);
+        sleep(Duration::from_millis(100));
+        println!("Hello from the second thread {}", user_t2.lock().unwrap().name);
     });
 
     t1.join().unwrap();
