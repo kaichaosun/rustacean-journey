d7a318a8ce39721f05bc0294100556713e5632cc
Kaicho Sun <kaichaosuna@gmail.com>
2022-06-27 11:42:25 +0800

    Arc to use shared value in multiple threads.

--- arc/src/main.rs
+++ arc/src/main.rs
@@ -1,15 +1,24 @@
-use std::thread::spawn;
+use std::{thread::spawn, sync::Arc};
 
 struct User {
     name: String,
 }
 
 fn main() {
-    let user = User {
+    let user_original = Arc::new(User {
         name: "kaichao".to_string(),
-    };
+    });
 
-    spawn(move || {
+    let user = user_original.clone();
+    let t1 = spawn(move || {
         println!("Hello from the first thread {}", user.name);
-    }).join().unwrap();
+    });
+
+    let user_t2 = user_original.clone();
+    let t2 = spawn(move || {
+        println!("Hello from the second thread {}", user_t2.name);
+    });
+
+    t1.join().unwrap();
+    t2.join().unwrap();
 }
